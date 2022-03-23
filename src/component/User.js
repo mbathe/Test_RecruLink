e19b9d272905690cc0b6184cdac24d4e9150338c
Paul MBATHE <mbathepaul@gmail.com>
2022-03-23 10:28:52 +0100

    Part 4

--- src/component/User.js
+++ src/component/User.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 import './User.css';
+import plus from './plus.png';
+import moins from './moins.png';
 
 function User(props) {
+  
+  const [expended,setExpended] = useState(false);
 
   return (
   
@@ -15,6 +19,23 @@ function User(props) {
         <p className='p'>Email: {props.user.email}</p>
         <p className='p'>City: {props.user.city}</p>
         <p className='p'>Country: {props.user.country}</p>
+
+        {expended && <div>
+          <br/>
+          <p className='p'>Test 1: 87%</p>
+          <p className='p'>Test 2: 100%</p>
+          <p className='p'>Test 3: 92%</p>
+          <p className='p'>Test 4: 86%</p>
+          <p className='p'>Test 5: 89%</p>
+          <p className='p'>Test 6: 88%</p>
+          <p className='p'>Test 7: 91%</p>
+          <p className='p'>Test 8: 87%</p>
+          
+        </div>}
+        
+      </div>
+      <div onClick={()=> setExpended(!expended)} className="expended" >
+          {expended===false? <img src={plus} className="image" alt='img' /> : <img src={moins} className="image" alt='img' /> }
       </div>
     </li>
     
